e14937475ccccab0ff4c6ac3942d02cd5f610668
diishqa <kkover073@gmail.com>
2025-09-21 10:00:43 +0300

    laba3_Kuzmina_ISP-232

---
+++ papka2/TwoProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TwoProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+        Console.WriteLine("Namespace: {0}, typeof(Program).Namespace ?? "<null>"");
+    }
+}
